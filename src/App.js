8104951defe35b3f0c91639c89f38fc211ba4897
Alisa Ivanenko <alisa.ivanenko@ynap.com>
2018-11-25 00:30:51 +0200

    added speech to text functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import * as tf from "@tensorflow/tfjs";
-import "./App.css";
-import Text2Speech from "./tools/Text2Speech";
-import { getRightWord } from "./tools/thesaurus";
-import * as rp from "request-promise-native";
+import React, {Component} from 'react';
+import * as tf from '@tensorflow/tfjs';
+import './App.css';
+import Text2Speech from './tools/Text2Speech';
+import {getRightWord} from './tools/thesaurus';
+import * as rp from 'request-promise-native';
+import SpeechRecorder from './tools/SpeechRecorder';
+import ToxicityScores from './components/ToxicityScores';
 import Speedometer from "./components/Speedometer";
-
-import ToxicityScores from "./components/ToxicityScores";
 
 class App extends Component {
   constructor() {
@@ -15,82 +15,79 @@ class App extends Component {
     this.state = {
       model: null,
       vocab: null,
-      data: this.initData
+      data: this.initData,
     };
 
-    this.someWord();
+    this.loadModel();
 
-    let greeting = "Me Fred. Me help you unswear";
+    this.someWord()
+
+    let greeting = 'Me Fred. Me help you unswear';
     this.text2Speech = new Text2Speech();
     this.text2Speech.speak(greeting);
 
     this.initializeState();
   }
 
-  someWord = async () => console.log(await getRightWord("pie"));
+  someWord = async () => console.log(await getRightWord('pie'))
 
-  initializeState = async () =>
-    await Promise.all([this.loadVocab(), this.loadModel()]);
+  initializeState = async () => await Promise.all([this.loadVocab(), this.loadModel()]);
 
   loadVocab = async () => {
     const options = {
-      uri: document.location.href + "/vocab.json",
-      json: true
+      uri: document.location.href + '/vocab.json',
+      json: true,
     };
 
     const vocab = await rp.get(options);
-    this.setState({ vocab });
+    debugger;
+    this.setState({vocab});
   };
 
   loadModel = async () => {
-    const loadedModel = await tf.loadModel(
-      process.env.PUBLIC_URL + "/model.json"
-    );
-    this.setState({ model: loadedModel });
+    const loadedModel = await tf.loadModel(process.env.PUBLIC_URL + '/model.json');
+    this.setState({model: loadedModel});
   };
 
-  onBlurText = event => {
-    if (event.target.value) this.text2Speech.speak(event.target.value);
+  onBlurText = (event) => {
+    if (event.target.value)
+      this.text2Speech.speak(event.target.value);
   };
 
-  onChangeText = event => {
-    this.setState({ text: event.target.value });
+  onChangeText = (event) => {
+    this.setState({text: event.target.value});
   };
 
   render() {
     const stateInitialized = this.state.model && this.state.vocab;
-    const Toxicity = chunkSize => {
-      const header = chunkSize
-        ? `Toxicity counts for chunk size ${chunkSize}`
-        : `Toxicity counts for unchunked input`;
+    const Toxicity = (chunkSize) => {
+      const header =
+        chunkSize ?
+          `Toxicity counts for chunk size ${chunkSize}` :
+          `Toxicity counts for unchunked input`;
       return (
         <div>
           <h3>{header}</h3>
           <ToxicityScores
             model={this.state.model}
             vocab={this.state.vocab}
-            text={this.state.text || ""}
+            text={this.state.text || ''}
             chunkSize={chunkSize}
           />
         </div>
       );
     };
     return (
-      <div className="App">
-        {stateInitialized ? (
+      <div className='App'>
+        {stateInitialized ?
           <div className="toxic-container">
             {Toxicity()}
             {Toxicity(1)}
             {Toxicity(10)}
-          </div>
-        ) : (
-          undefined
-        )}
-        <input
-          type="text"
-          onBlur={this.onBlurText}
-          onChange={this.onChangeText}
-        />
+          </div> :
+          undefined}
+        <input type='text' onBlur={this.onBlurText} onChange={this.onChangeText}/>
+        <SpeechRecorder/>
         <Speedometer />
       </div>
     );
