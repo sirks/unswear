65d0bbdb8f16e2e893e47a157bc631ea02f69d05
Jani Mäkelä <janimakelaa@gmail.com>
2018-11-25 00:47:49 +0200

    Clean up SpeechRecorder code

--- src/tools/SpeechRecorder.jsx
+++ src/tools/SpeechRecorder.jsx
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-//import * as speech from '@google-cloud/speech'; // Imports the Google Cloud client library
-//import gapi from 'gapi-client';
 import SpeechToText from 'speech-to-text';
 
 class SpeechRecorder extends Component {
@@ -8,9 +6,9 @@ class SpeechRecorder extends Component {
   constructor() {
     super();
     this.constraints = {audio: true, video: false};
-    this.state = {listener: null, isStarted: false, textFromMic: "", allText: ""};
-
-    //listener.recognition.onend = this.onEndReplaced;
+    this.state = {isStarted: false, textFromMic: "", allText: ""};
+    this.listener = null;
+    this.stopped = true;
   }
 
   onFinalised = text => {
@@ -24,36 +22,30 @@ class SpeechRecorder extends Component {
   }
 
   onEndReplaced = () => {
-    this.onBtnRecordClicked();
     console.log("onEndReplaced called");
+    if (!this.stopped) {
+      console.log("restarting listener");
+      this.onBtnRecordClicked();
+    }
   }
 
   createNewListener = () => {
-    const listener = new SpeechToText(this.onFinalised, this.onAnythingSaid);
-    this.setState({listener: listener});
-    return listener;
+    this.listener = new SpeechToText(this.onFinalised, this.onAnythingSaid);
+    this.listener.recognition.onend = this.onEndReplaced;
   }
 
   onBtnRecordClicked = () => {
     console.log("start clicked");
-    const listener = this.createNewListener();
-    listener.startListening();
+    this.stopped = false;
+    this.createNewListener();
+    this.listener.startListening();
     console.log(this.state.listener);
-    //
-    // if (this.state.isStarted) {
-    //   this.state.listener.restart();
-    //   console.log('should restart? ' + this.state.listener.shouldRestart)
-    // }
-    // else {
-    //   this.state.listener.startListening();
-    //   this.setState({isStarted: true});
-    //   console.log('should restart? ' + this.state.listener.shouldRestart)
-    // }
   }
 
   onBtnStopClicked = () => {
     console.log("stop clicked");
-    this.state.listener.stopListening();
+    this.stopped = true;
+    this.listener.stopListening();
   }
 
   render() {
@@ -67,12 +59,6 @@ class SpeechRecorder extends Component {
 
     );
   }
-
-  componentDidMount() {
-    navigator.mediaDevices.getUserMedia(this.constraints)
-    .then(stream => this.setState({stream}))
-  }
 }
-
 
 export default SpeechRecorder;
