b9ae592589e1a8a25568ac7c706b6e90e0e70f8f
kriss <kriss.pujats@gmail.com>
2018-11-24 23:08:16 +0200

    toxicity

--- src/App.js
+++ src/App.js
@@ -1,95 +1,47 @@
 import React, {Component} from 'react';
-import * as tf from '@tensorflow/tfjs';
 import './App.css';
 import Text2Speech from './tools/Text2Speech';
+import Toxicity from "./tools/Toxicity";
 import {getRightWord} from './tools/thesaurus';
-import * as rp from 'request-promise-native';
-import SpeechRecorder from './tools/SpeechRecorder';
-import ToxicityScores from './components/ToxicityScores';
 
 class App extends Component {
   constructor() {
     super();
 
-    this.state = {
-      model: null,
-      vocab: null,
-      data: this.initData,
-    };
-
-    this.loadModel();
-
-    this.someWord()
-
-    let greeting = 'Me Fred. Me help you unswear';
+    let greeting = 'Hi. I am Fred. Prepare your prayers to get unsweared';
     this.text2Speech = new Text2Speech();
     this.text2Speech.speak(greeting);
 
-    this.initializeState();
+    this.toxicity = new Toxicity();
   }
 
-  someWord = async () => console.log(await getRightWord('pie'))
+  onChangeText = async (event) => {
+    const text = ' ' + event.target.value;
+    if (!text.endsWith(' ')) {
+      return;
+    }
+    const regex = / ([a-z]|[A-Z])+ $/g;
+    const lastWord = text.match(regex)[0].trim();
+    let {wordScore, totalScore} = await this.toxicity.addWord(lastWord);
+    if (wordScore > 0.5) {
+      debugger;
+      this.text2Speech.speak(lastWord);
+    }
+    if (totalScore > 0.1) {
+      debugger;
+      this.text2Speech.speak("bugger");
+    }
 
-  initializeState = async () => await Promise.all([this.loadVocab(), this.loadModel()]);
+  someWord = async () => console.log(await getRightWord('pie'));
 
-  loadVocab = async () => {
-    const options = {
-      uri: document.location.href + '/vocab.json',
-      json: true,
-    };
-
-    const vocab = await rp.get(options);
-    debugger;
-    this.setState({vocab});
-  };
-
-  loadModel = async () => {
-    const loadedModel = await tf.loadModel(process.env.PUBLIC_URL + '/model.json');
-    this.setState({model: loadedModel});
-  };
-
-  onBlurText = (event) => {
-    if (event.target.value)
-      this.text2Speech.speak(event.target.value);
-  };
-
-  onChangeText = (event) => {
-    this.setState({text: event.target.value});
-  };
-
-  render() {
-    const stateInitialized = this.state.model && this.state.vocab;
-    const Toxicity = (chunkSize) => {
-      const header =
-        chunkSize ?
-          `Toxicity counts for chunk size ${chunkSize}` :
-          `Toxicity counts for unchunked input`;
+  render(){
       return (
-        <div>
-          <h3>{header}</h3>
-          <ToxicityScores
-            model={this.state.model}
-            vocab={this.state.vocab}
-            text={this.state.text || ''}
-            chunkSize={chunkSize}
-          />
+        <div className='App'>
+          <input type='text' onChange={this.onChangeText}/>
+          <SpeechRecorder/>
         </div>
       );
-    };
-    return (
-      <div className='App'>
-        {stateInitialized ?
-          <div className="toxic-container">
-            {Toxicity()}
-            {Toxicity(1)}
-            {Toxicity(10)}
-          </div> :
-          undefined}
-        <input type='text' onBlur={this.onBlurText} onChange={this.onChangeText}/>
-        <SpeechRecorder/>
-      </div>
-    );
-  }
+    }
 }
 
 export default App;
---
+++ src/tools/Toxicity.js
@@ -0,0 +1,57 @@
+import * as tf from '@tensorflow/tfjs';
+import * as rp from "request-promise-native";
+
+
+class Toxicity {
+  constructor() {
+    this.totalScore = 0;
+    this.wordCount = 0;
+
+    this.vocabulary = null;
+    this.model = null;
+    this.load();
+  }
+
+  async load() {
+    await Promise.all([this.loadVocabulary(), this.loadModel()]);
+  }
+
+  async loadVocabulary() {
+    const options = {
+      uri: document.location.href + '/vocab.json',
+      json: true,
+    };
+
+    this.vocabulary = await rp.get(options);
+  };
+
+  async loadModel() {
+    this.model = await tf.loadModel(process.env.PUBLIC_URL + '/model.json');
+  }
+
+  ready() {
+    return this.vocabulary && this.model
+  }
+
+  async testWord(word) {
+    const tensorBuffer = tf.zeros([1, 100]).buffer();
+    if (this.vocabulary.hasOwnProperty(word)) {
+      tensorBuffer.set(this.vocabulary[word], 0, 0);
+    }
+    return (await this.model.predict(tensorBuffer.toTensor()).data())[0];
+  }
+
+  async addWord(word) {
+    let wordScore = await this.testWord(word);
+    this.totalScore = (this.totalScore * this.wordCount + wordScore) / ++this.wordCount;
+    return {wordScore, totalScore: this.totalScore}
+  }
+
+  reset() {
+    this.totalScore = 0;
+    this.wordCount = 0;
+  }
+
+}
+
+export default Toxicity
