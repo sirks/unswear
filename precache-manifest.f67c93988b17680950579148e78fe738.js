087e534002286c949398229337e4e8376f8a6a99
kriss <kriss.pujats@gmail.com>
2018-11-24 14:18:32 +0200

    Updates

---
+++ precache-manifest.f67c93988b17680950579148e78fe738.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e01e6b66a4a77caffeaf",
+    "url": "/unswear/static/js/runtime~main.e01e6b66.js"
+  },
+  {
+    "revision": "cb0aa41e1200078bdc8f",
+    "url": "/unswear/static/js/main.cb0aa41e.chunk.js"
+  },
+  {
+    "revision": "668f14bd8121ffbc034c",
+    "url": "/unswear/static/js/1.668f14bd.chunk.js"
+  },
+  {
+    "revision": "cb0aa41e1200078bdc8f",
+    "url": "/unswear/static/css/main.c49bd63d.chunk.css"
+  },
+  {
+    "revision": "b8b66d0010bf1447246eb82c9770351c",
+    "url": "/unswear/index.html"
+  }
+];
