df812dbdbc9d615caa1eef6b051d71dbeb7144bf
Christian Hamamdjian <christian.hamamdjian@welho.com>
2018-11-25 02:41:19 +0200

    Logo and button styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
-import './App.css';
-import Text2Speech from './tools/Text2Speech';
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./assets/logo.png";
+import Text2Speech from "./tools/Text2Speech";
 import Toxicity from "./tools/Toxicity";
-import {getSynonym} from './tools/thesaurus';
+import { getSynonym } from "./tools/thesaurus";
 import SpeechRecorder from "./tools/SpeechRecorder";
 import Speedometer from "./components/Speedometer";
-
 
 const TOXIC_THRESHOLD = 0.33;
 
@@ -14,38 +14,41 @@ class App extends Component {
     super();
 
     this.state = {
-      level: 0,
+      level: 0
     };
 
     // let greeting = 'Hi. I am Fred. Prepare your prayers to get unsweared';
-    let greeting = 'Hi';
+    let greeting = "Hi";
     this.text2Speech = new Text2Speech();
     this.text2Speech.speak(greeting);
 
     this.toxicity = new Toxicity();
   }
 
-  onChangeText = async (event) => {
-    const text = ' ' + event.target.value;
-    if (!text.endsWith(' ')) {
+  onChangeText = async event => {
+    const text = " " + event.target.value;
+    if (!text.endsWith(" ")) {
       return;
     }
     const regex = / ([a-z]|[A-Z])+ $/g;
     const lastWord = text.match(regex)[0].trim();
-    let {wordScore, totalScore} = await this.toxicity.addWord(lastWord);
+    let { wordScore, totalScore } = await this.toxicity.addWord(lastWord);
     console.log(wordScore, totalScore);
     if (wordScore > TOXIC_THRESHOLD) {
       this.text2Speech.speak(await getSynonym(lastWord));
     }
-    this.setState({level: totalScore})
+    this.setState({ level: totalScore });
   };
 
   render() {
     return (
-      <div className='App'>
-        <textarea onChange={this.onChangeText}/>
-        <SpeechRecorder/>
-        <Speedometer level={this.state.level}/>
+      <div className="App">
+        <div className="logo-container">
+          <img src={logo} alt="" />
+        </div>
+        <Speedometer level={this.state.level} />
+        <textarea onChange={this.onChangeText} />
+        <SpeechRecorder />
       </div>
     );
   }
