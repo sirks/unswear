2dfdff62e2c0656f53380c733a9175d5e16968a4
kriss <kriss.pujats@gmail.com>
2018-11-26 18:01:02 +0200

    flatmap shim

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// shims
+if (!Array.prototype.hasOwnProperty('flatMap')) {
+  Array.prototype.flatMap = function (f) {
+    return this.map(f).reduce((x, y) => x.concat(y), []);
+  }
+}
